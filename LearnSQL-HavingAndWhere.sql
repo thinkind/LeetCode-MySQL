396b5a71ce4100d41905b2f09b50bd5e8315d2c8
James Zhao <James.vc@outlook.com>
2019-08-23 15:55:35 +0800

    Create LearnSQL-HavingAndWhere.sql

---
+++ LearnSQL-HavingAndWhere.sql
@@ -0,0 +1,9 @@
+-- MySQL
+-- Having and WHERE.sql
+-- 具有两个以上产品且其价格大于等于4的供应商
+
+SELECT vend_id, COUNT(*) AS num_prods
+    FROM Products
+    WHERE prod_price >= 4   -- 数据分组前进行过滤
+    GROUP BY vend_id
+    HAVING COUNT(*) >= 2;   -- 数据分组后进行过滤
