dd72b8efcf5bbe67d66a830f2f89b8e4db509aba
James Zhao <James.vc@outlook.com>
2019-09-16 10:07:07 +0800

    Update LearnSQL_Update.sql

--- Src_Learning/LearnSQL_Update.sql
+++ Src_Learning/LearnSQL_Update.sql
@@ -1,5 +1,5 @@
 -- MySQL
 
-UPDATE Customers                            -- DELETE检查是否有WHERE，使用前先用SELECT测试
+UPDATE Customers                            -- UPDATE和DELETE都检查：是否有WHERE；使用前先用SELECT测试
     SET cust_email = 'James.vc@outlook.com'
     WHERE cust_id = '024';
